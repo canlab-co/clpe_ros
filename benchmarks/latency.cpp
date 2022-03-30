3508dc4d0f8452d686197a06075ef893bd05970f
Teo Koon Peng <koonpeng@openrobotics.org>
2022-03-30 16:27:32 +0800

    Revert "ros1 benchmark"
    This reverts commit a0ab659aed3cc702be699b44bf8f69c46ed94562.

--- benchmarks/latency.cpp
+++ benchmarks/latency.cpp
@@ -1,60 +1,34 @@
-/*
- * Copyright (C) 2022 [PLACEHOLDER]
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
-
-#include <ros/ros.h>
-
+#include <builtin_interfaces/msg/time.hpp>
 #include <iomanip>
-#include <sensor_msgs/Image.h>
+#include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/image.hpp>
 
-//==============================================================================
+rcl_time_point_value_t from_ros_time(const builtin_interfaces::msg::Time & ros_time)
+{
+  return static_cast<int64_t>(ros_time.sec) * 1000000000 + ros_time.nanosec;
+}
+
 int main(int argc, char * argv[])
 {
-  ros::init(argc, argv, "clpe");
+  rclcpp::init(argc, argv);
 
-  const std::size_t cam_count = 4;
+  auto node = rclcpp::Node::make_shared("canlab_benchmark_latency");
 
-  auto node = ros::NodeHandle("canlab_benchmark_latency");
-  std::array<std::size_t, cam_count> cam_sub_count;
-  // Latency in millis
-  std::array<double, cam_count> avg_latencies;
-  for (auto i = 0; i < cam_count; ++i) {
-    cam_sub_count[i] = 1;
-    avg_latencies[i] = 0.0;
-  }
-
-  std::array<ros::Subscriber, cam_count> subs;
-  using sensor_msgs::Image;
-  for (int i = 0; i < cam_count; ++i) {
-    subs[i] = node.subscribe<Image>(
-        "cam_" + std::to_string(i) + "/image_raw", 10,
-        [i, &node, &cam_sub_count, &avg_latencies](const Image::ConstPtr& image) {
-          const auto local_time = ros::Time::now().nsec;
-          const auto cam_time = image->header.stamp.nsec;
+  std::array<rclcpp::SubscriptionBase::SharedPtr, 4> subs;
+  using sensor_msgs::msg::Image;
+  for (int i = 0; i < 4; ++i) {
+    subs[i] = node->create_subscription<Image>(
+        "cam_" + std::to_string(i) + "/image_raw", rclcpp::SystemDefaultsQoS(),
+        [i, &node](const Image::SharedPtr image) {
+          const auto local_time = node->get_clock()->now().nanoseconds();
+          const auto cam_time = from_ros_time(image->header.stamp);
           const auto diff_ns = local_time - cam_time;
-          const double latency = diff_ns / 1000000.0;
-          avg_latencies[i] =
-              (avg_latencies[i] * cam_sub_count[i] + latency) / (cam_sub_count[i] + 1);
-          cam_sub_count[i]++;
           std::cout << "cam_" << i << " latency = " << std::fixed << std::setprecision(3)
-                    << avg_latencies[i] << "ms" << std::endl;
+                    << diff_ns / 1000.0 << "us" << std::endl;
         });
   }
 
-  ros::spin();
+  rclcpp::spin(node);
 
   return 0;
 }
